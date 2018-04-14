582a96fe08792c27b2640e067dd7a7b5ee72a48f
Jonatan Zint <j.zint@frykk.de>
2018-04-14 10:15:14 +0200

    Initial commit

---
+++ config/default.py
@@ -0,0 +1,4 @@
+DEBUG = False
+MAIL_SENDER = 'not@set.com'
+MAIL_RECIPIENTS = []
+THANKYOU_PAGE_DEFAULT = 'http://www.ihasabucket.com/'
---
+++ formidable.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+from os import environ
+
+app = Flask(__name__)
+app.config.from_object('config.default')
+settings = environ.get('FORMIDABLE_SETTINGS_MODULE', None)
+if settings:
+    app.config.from_object(settings)
+
+mail = Mail(app)
+
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    referrer = request.referrer
+    next_page = request.form.get('next_page', app.config.get('THANKYOU_PAGE_DEFAULT'))
+    originator = request.form.get('email', 'unknown')
+    mailtext = render_template('new_submit_mail.txt', form=request.form, referrer=request.referrer)
+    message = Message(body=mailtext,
+                      subject='New form submission on {} from {}'.format(referrer, originator),
+                      sender=app.config.get('MAIL_SENDER'),
+                      recipients=app.config.get('MAIL_RECIPIENTS'),
+                      reply_to=originator if originator != 'unknown' else None
+                      )
+    print(app.config)
+    mail.send(message)
+
+    return redirect(next_page)
